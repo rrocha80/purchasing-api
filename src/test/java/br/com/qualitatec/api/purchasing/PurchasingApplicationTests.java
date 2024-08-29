420b481640776cb1f18f14034aebc4834a89cbfe
Rodrigo Rocha <rodrigo.lyan@gmail.com>
2024-08-29 16:30:26 -0300

    Commit inicial

---
+++ src/test/java/br/com/qualitatec/api/purchasing/PurchasingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qualitatec.api.purchasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
